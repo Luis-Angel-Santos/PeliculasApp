06b8efa4227d15d9cd0b7b6cb412313f1ffce651
Luis Angel <santosangel748@gmail.com>
2020-10-18 18:59:13 -0500

    Actores de la pelicula y manejo de errores

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PeliculasService } from '../../services/peliculas.service';
 import { MovieDetailsResponse } from '../../interfaces/movie-response';
 import { Movie } from '../../interfaces/cartelera-response';
 import { Location } from '@angular/common';
+import { Cast } from '../../interfaces/credits-response';
 
 @Component({
   selector: 'app-pelicula',
@@ -13,15 +14,28 @@ import { Location } from '@angular/common';
 export class PeliculaComponent implements OnInit {
 
   public pelicula: MovieDetailsResponse;
+  public cast: Cast[];
+
 
   constructor(private activatedRouter: ActivatedRoute,
               private peliculasService: PeliculasService,
-              private location: Location) { }
+              private location: Location,
+              private router: Router) { }
 
   ngOnInit(): void {
     const { id }=this.activatedRouter.snapshot.params;
     this.peliculasService.getPeliculaDetalle(id).subscribe(movie=>{
       this.pelicula=movie;
+      if (!movie) {
+        this.router.navigateByUrl('/home');
+        return;
+      }
+      this.pelicula=movie;
+    });
+    
+
+    this.peliculasService.getCast(id).subscribe(cast=>{
+      this.cast=cast;
     })
   
   }
--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { CarteleraResponse, Movie } from '../interfaces/cartelera-response';
-import { map, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 import { MovieDetailsResponse } from '../interfaces/movie-response';
+import { CreditsResponse } from '../interfaces/credits-response';
 
 @Injectable({
   providedIn: 'root'
@@ -61,7 +62,18 @@ export class PeliculasService {
   getPeliculaDetalle(id: string){
     return this.http.get<MovieDetailsResponse>(`${this.baseUrl}/movie/${id}`,{
       params: this.params
-    });
+    }).pipe(
+      catchError(err=>of(null))
+    )
+  }
+
+  getCast(id: string){
+    return this.http.get<CreditsResponse>(`${this.baseUrl}/movie/${id}/credits`,{
+      params: this.params
+    }).pipe(
+      map(resp=>resp.cast),
+      catchError(err=>of([]))
+    );
   }
 
 } 

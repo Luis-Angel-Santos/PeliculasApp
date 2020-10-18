809f8e5b5c91b795eac4f76b08ec9f421248af7b
Luis Angel <santosangel748@gmail.com>
2020-10-17 21:08:43 -0500

    Colocando las estrellas de las peliculas

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,6 +4,8 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
+import { RatingComponent } from 'ng-starrating/lib/rating.component';
+import { RatingModule } from 'ng-starrating';
 
 
 
@@ -20,7 +22,8 @@ import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    RatingModule
   ]
 })
 export class ComponentsModule { }

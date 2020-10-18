2ce8c4f2f378573dd45e9affc69f0785f12aafc0
Luis Angel <santosangel748@gmail.com>
2020-10-18 18:43:37 -0500

    Diseño de la pantalla de detalle

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PeliculasService } from '../../services/peliculas.service';
+import { MovieDetailsResponse } from '../../interfaces/movie-response';
+import { Movie } from '../../interfaces/cartelera-response';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-pelicula',
@@ -9,16 +12,23 @@ import { PeliculasService } from '../../services/peliculas.service';
 })
 export class PeliculaComponent implements OnInit {
 
+  public pelicula: MovieDetailsResponse;
+
   constructor(private activatedRouter: ActivatedRoute,
-              private peliculasService: PeliculasService) { }
+              private peliculasService: PeliculasService,
+              private location: Location) { }
 
   ngOnInit(): void {
-    const { id }=this.activatedRouter.snapshot.params.id;
+    const { id }=this.activatedRouter.snapshot.params;
     this.peliculasService.getPeliculaDetalle(id).subscribe(movie=>{
-      
+      this.pelicula=movie;
     })
   
   }
 
+  onRegresar(){
+    this.location.back();
+  }
 
 }
+
